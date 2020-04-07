39780a25a70fe6b13d6771d7b4830ca7f342edf3
Wolfy <wolfy3543@gmail.com>
2020-04-07 03:56:16 -0400

    oops

--- config.js
+++ config.js
@@ -6,5 +6,5 @@ module.exports = Object.seal({
     "defaultRoomColor": "#3b5054",
     "defaultLobbyColor": "#19b4b9",
     "defaultLobbyColor2": "#801014",
-    "adminpass": "27PP6YLTxg0b1P2B8eGSOki1"
+    "adminpass": "adminpass"
 })
