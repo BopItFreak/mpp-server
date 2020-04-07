f6e52ccda05932cc621f4b32f1896439f641028c
wolfy01 <45273698+wolfy01@users.noreply.github.com>
2020-04-07 02:44:34 -0400

    Update and rename config.json to config.js
    Add quotas

---
+++ src/db/config.js
@@ -0,0 +1,34 @@
+module.exports = Object.seal({
+    "port": "8080",
+    "motd": "You agree to read this message.",
+    "_id_PrivateKey": "boppity",
+    "defaultUsername": "Anonymous",
+    "defaultRoomColor": "#3b5054",
+    "defaultLobbyColor": "#19b4b9",
+    "defaultLobbyColor2": "#801014",
+    "adminpass": "adminpass_h4gKJCX2",
+    "quotas":{
+        "chat":{
+            "amount": 4,
+            "time": 4000
+        },
+        "name":{
+            "amount": 30,
+            "time": 30 * 60000
+        },
+        "room":{
+            "time": 500
+        },
+        "cursor":{
+            "time": 16
+        },
+        "kickban":{
+            "amount": 2,
+            "time": 1000
+        },
+        "crowned_chat":{
+            "amount": 10,
+            "time": 4000
+        }
+    }
+})
