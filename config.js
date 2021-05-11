1b422e68ad55efe30432809668bdc747d578f4ab
Hri7566 <hri7566@gmail.com>
2021-05-10 20:15:59 -0400

    remove unused settings

--- config.js
+++ config.js
@@ -3,7 +3,6 @@ module.exports = Object.seal({
     motd: "You agree to read this message.",
     _id_PrivateKey: "boppity",
     defaultUsername: "Anonymous",
-    defaultRoomColor: "#3b5054",
     defaultLobbyColor: "#19b4b9",
     defaultLobbyColor2: "#801014",
     adminpass: "adminpass",
